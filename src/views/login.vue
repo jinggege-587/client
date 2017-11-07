ac55de0cfb510bc56e2ccbc37bafff4443a5ba14
靖汉宇 <jinhanyu@raycloud.com>
2017-11-07 16:20:47 +0800

    no message

---
+++ src/views/login.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="login">
+    <div class="page-wrap">
+      <div class="page-title">
+        <a @click="$router.go(-1)">
+          <i class="icon-back"></i>
+        </a>
+        <span>登录</span>
+      </div>
+      <div class="page-content">
+        <mt-field 
+          style="margin-top: 10px;border-top: 1px solid #ccc" 
+          label="账户" 
+          placeholder="请输入账户" 
+          :attr="{
+            maxLength: 11
+          }"
+          :state="fieldState.identifier" 
+          v-model="loginForm.identifier" 
+          @blur.native.capture="check('identifier')"
+          >
+        </mt-field>
+        <mt-field 
+          label="密码"
+          style="border-bottom: 1px solid #ccc"
+          placeholder="请输入登录密码"
+          :attr="{
+            maxLength: 20
+          }"
+          type="password" 
+          :state="fieldState.password" 
+          v-model="loginForm.password">
+        </mt-field>
+        <div style="margin: 10px 0;text-align: center; font-size: 12px;">
+          <div style="">
+            <input style="margin: 1px 0;width: 15px;height:15px;" type="checkbox" v-model="loginForm.validate">
+            <span style="vertical-align: top;color: #888;">注册或使用网站账号登录即视为您已同意</span>
+          </div>
+          <div style="font-size: 14px;">
+            <router-link tag="span" :to="{path: '/agreement'}">《用户使用协议》</router-link>
+          </div>
+        </div>
+        <div style="padding: 0px 10px;">
+          <mt-button style="width:100%;margin: 20px 0 10px 0;background-color:red;color:#ffffff;" @click="sumbit('loginForm')">登录</mt-button>
+          <a href="javascript: void(0);" style="color:#aaa;font-size: 14px;" @click="$router.replace({name: 'smsLogin'})">→使用短信验证码登录</a>
+          <mt-button style="width:100%;margin-top: 20px;border-bottom-color:red;color:red;" plain @click="$router.push({name: 'register'})">手机号快速注册</mt-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      loginForm: {
+        identifier: '',
+        password: '',
+        validate: true
+      },
+      fieldState: {
+        identifier: '',
+        password: ''
+      },
+      toastClient: ''
+    }
+  },
+  created () {
+  },
+  activated () {
+  },
+  methods: {
+    check (fieldName) {
+      if (!this.loginForm[fieldName]) {
+        this.fieldState[fieldName] = 'error'
+      } else {
+        this.fieldState[fieldName] = 'success'
+      }
+    },
+    validateField (field, scope) {
+      let valid = true
+      switch (field) {
+        case 'identifier':
+          if (!scope.identifier.trim()) {
+            this.toast('请输入账号')
+            valid = false
+          }
+          break
+        case 'password':
+          if (!scope.password.trim()) {
+            this.toast('请输入密码')
+            valid = false
+          }
+          break
+        case 'validate':
+          if (!scope.validate) {
+            this.toast('请勾选同意')
+            valid = false
+          }
+          break
+        default:
+          break
+      }
+      this.check(field)
+      return valid
+    },
+    sumbit (formName) {
+      let valid = true
+      if (formName === 'loginForm') {
+        for (let k in this.loginForm) {
+          valid = this.validateField(k, this.loginForm)
+          if (!valid) {
+            return
+          }
+        }
+        if (valid) {
+          this.$api.login(this.loginForm, data => {
+            window.localStorage.setItem('expirationTime', data.resource.expirationTime)
+            window.localStorage.setItem('token', data.resource.token)
+            window.localStorage.setItem('identifier', this.loginForm.identifier)
+            this.toast('登录成功')
+            this.$router.replace({name: 'userCenter'})
+          }, e => {
+            this.toast(`登录失败,${e.message}`)
+          })
+        }
+      }
+    },
+    toast (message) {
+      if (this.toastClient) {
+        this.toastClient.close()
+      }
+      this.toastClient = this.$toast({
+        message: message,
+        position: 'middle',
+        duration: 2000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/message.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="message">
+    <div class="page-wrap">
+      <div class="page-title">
+        <a @click="routerBack">
+          <i class="icon-back"></i>
+        </a>
+        <span>公告消息</span>
+      </div>
+      <div class="page-content">
+        <div class="media-list">
+          <router-link :to="{name: 'messageList', query: {type: 'system'}}" class="media-item">
+            <div class="media-img">
+              <img src="static/icon/icon-item-bell.svg" alt="">
+            </div>
+            <div class="media-title">
+              <h5>系统公告
+                <!-- <span>2017-07-27</span> -->
+              </h5>
+              <!-- <p>关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告</p> -->
+              <div class="circle-red"></div>
+            </div>
+          </router-link>
+          <router-link :to="{name: 'messageList', query: {type: 'personal'}}" class="media-item">
+            <div class="media-img">
+              <img src="static/icon/icon-item-paper.svg" alt="">
+            </div>
+            <div class="media-title">
+              <h5>个人通知
+                <!-- <span>2017-07-27</span> -->
+              </h5>
+              <!-- <p>关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告关于发货时间调整的公告</p> -->
+              <div class="circle-red"></div>
+            </div>
+          </router-link>
+          <!-- <router-link :to="{name: 'messageInfo'}" class="media-item">
+            <div class="media-img">
+              <img src="static/icon/icon-item-paper.svg" alt="">
+            </div>
+            <div class="media-title">
+              <h5>拍币说明
+                <span>2017-07-27</span>
+              </h5>
+              <p>关于充值拍币后不可退回的说明</p>
+              <div class="circle-red"></div>
+            </div>
+          </router-link> -->
+        </div>
+        <!-- <div class="media-list">
+          <router-link :to="{name: 'messageInfo'}" class="media-item">
+            <div class="media-img">
+              <img src="static/icon/icon-item-gift.svg" alt="">
+            </div>
+            <div class="media-title">
+              <h5>中奖消息</h5>
+              <p>恭喜你中得iPhone 7s Plus 一部，请尽快下单。</p>
+              <div class="circle-red"></div>
+            </div>
+          </router-link>
+          <router-link :to="{name: 'messageInfo'}" class="media-item">
+            <div class="media-img">
+              <img src="static/icon/icon-item-car.svg" alt="">
+            </div>
+            <div class="media-title">
+              <h5>发货消息</h5>
+              <p>您拍中的商品正搭快车赶往您身边</p>
+              <div class="circle-red"></div>
+            </div>
+          </router-link>
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    routerBack () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style>
+.media-list {
+  width: 100%;
+  padding: 0;
+  margin-bottom: 10px;
+  border-top: 1px solid #f2f2f2;
+}
+
+a.media-item {
+  display: inline-block;
+  margin: 0 0 -4px 0;
+  padding: 0;
+  width: 100%;
+  position: relative;
+  border-bottom: 1px solid #f2f2f2;
+  background-color: #FFFFFF;
+}
+
+a.media-item .media-img {
+  width: 15%;
+  float: left;
+}
+
+a.media-item .media-img img {
+  margin: 10px 0 5px 10px;
+  min-width: 24px;
+  max-width: 56px;
+}
+
+.media-title {
+  width: 85%;
+  float: left;
+}
+
+.media-title h5 {
+  padding: 14px 10px 5px 10px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #333333;
+}
+
+.media-title p {
+  padding: 0 30px 10px 10px;
+  white-space: nowrap;
+  word-wrap: normal;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 12px;
+  color: #999999;
+}
+
+.media-title span {
+  float: right;
+  font-weight: 300;
+  font-size: 12px;
+  color: #999999;
+  vertical-align: middle;
+}
+
+div.circle-red {
+  width: 10px;
+  height: 10px;
+  border-radius: 30px;
+  background-color: #ef0000;
+  position: absolute;
+  right: 10px;
+  top: 38px;
+}
+</style>
